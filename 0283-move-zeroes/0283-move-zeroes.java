272a6d458e066921a35e201f17e6bc4cd5735b6b
antar-dev1902 <antarjot3061.beai25@chitkara.edu.in>
2026-08-15 00:59:36 +0530

    Time: 2 ms (91.97%), Space: 47.4 MB (93.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int j=0;
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==0 & nums[j]!=0){
-                    nums[i]=nums[j];
-                    nums[j]=0;
-
-
-                }
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
             }
+
+        }
+        while(j<nums.length){
+            nums[j]=0;
+            j++;
         }
         
         
