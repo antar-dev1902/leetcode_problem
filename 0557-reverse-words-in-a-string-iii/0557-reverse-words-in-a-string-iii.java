10dc39fe7d6718379166d9099a9db22c0d592109
antar-dev1902 <antarjot3061.beai25@chitkara.edu.in>
2026-08-17 02:08:53 +0530

    Time: 5 ms (53.06%), Space: 46.9 MB (50.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,18 +1,18 @@
 class Solution {
     public String reverseWords(String s) {
         String[] a=s.split(" ");
+        StringBuilder b=new StringBuilder();        
         for(int i=0;i<a.length;i++){
             StringBuilder sb=new StringBuilder(a[i]);
             a[i]=sb.reverse().toString();
-        }
-        String b="";
-        for(int i=0;i<a.length;i++){
-            b+=a[i];
+            b.append(a[i]);
             if(i<a.length-1){
-                b+=" ";
+                b.append(" ");
             }
         }
-        return b;
+        
+        
+        return b.toString();
         
     }
 }
