c1cc7446ec48627da9bbf369fb722988cf57c311
antar-dev1902 <antarjot3061.beai25@chitkara.edu.in>
2026-08-27 02:57:09 +0530

    Time: 1 ms (27.56%), Space: 43.5 MB (5.92%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.strip();
+        String[] arr=s.split(" ");
+        return arr[arr.length-1].length();
+        
+    }
+}
