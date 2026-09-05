4864702224223a67202ad247cd35531702b43031
antar-dev1902 <antarjot3061.beai25@chitkara.edu.in>
2026-09-06 04:22:05 +0530

    Time: 8 ms (74.1%), Space: 47.6 MB (36.33%) - LeetHub

--- 2788-split-strings-by-separator/2788-split-strings-by-separator.java
+++ 2788-split-strings-by-separator/2788-split-strings-by-separator.java
@@ -1,13 +1,17 @@
 class Solution {
     public List<String> splitWordsBySeparator(List<String> words, char separator) {
-        int a=words.size();
-            for(int i=0;i<a;i++){
-                String b= words.remove(0);
-                String[] c=b.split("\\Q" + separator + "\\E");
-                Collections.addAll(words,c);
+        List<String> ans = new ArrayList<>();
+
+        for (String word : words) {
+            String[] parts = word.split("\\" + separator);
+
+            for (String part : parts) {
+                if (!part.isEmpty()) {
+                    ans.add(part);
+                }
             }
-        words.removeIf(x -> x.isEmpty());    
-        return words;    
-        
+        }
+
+        return ans;
     }
 }
