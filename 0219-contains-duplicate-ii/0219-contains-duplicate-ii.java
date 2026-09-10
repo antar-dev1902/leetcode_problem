c5312a1be2df4d22da3433f65e77cf519c1cc4cb
antar-dev1902 <274545409+antar-dev1902@users.noreply.github.com>
2026-09-10 19:01:22 +0530

    Time: 35 ms (10.29%), Space: 124.9 MB (11.19%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> set=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.containsKey(nums[i])){
+                if(Math.abs(i-set.get(nums[i]))<=k){
+                    return true;
+                }
+            }
+            set.put(nums[i],i);
+        }
+        return false;
+        
+    }
+}
