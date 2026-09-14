176e72e23273371312d5a7a8c21fb548559798c7
antar-dev1902 <274545409+antar-dev1902@users.noreply.github.com>
2026-09-15 02:03:33 +0530

    Time: 7 ms (79.22%), Space: 47.1 MB (26.22%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        HashSet<Character> set=new HashSet<>();
+        for(char c:s.toCharArray()){
+            set.add(c);
+        }
+        return set.size();
+        
+    }
+}
